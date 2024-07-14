96944b05384c266d28ce0d483459aa95a5e21512
Ken20215 <k.ishioka@renk.co.jp>
2024-07-14 16:26:25 +0900

    現在位置を取得して天気情報を取得

--- WeatherApiApp/WeatherApiApp/ViewController/WeatherViewController.swift
+++ WeatherApiApp/WeatherApiApp/ViewController/WeatherViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import CoreLocation // 位置情報を取得するために必要なモジュールをインポートする。
 
 class WeatherViewController: UIViewController {
     
@@ -17,17 +18,37 @@ class WeatherViewController: UIViewController {
     @IBOutlet private var taikan: UILabel!
     @IBOutlet private var humidity: UILabel!
     @IBOutlet private var wind: UILabel!
+    private var currentLocation: CLLocation?
+    // 位置情報を取得するために必要なプロトコルを付与する。
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getWeatherData(zip: "950-0086", country: "JP")
+        self.locationManager.delegate = self // CLLocationManagerDelegateプロトコルを使用するためにdelegateをViewControllerクラスに設定する。
+        locationManager.requestWhenInUseAuthorization() // 位置情報の許可設定を通知する。
+    }
+    
+    @IBAction func getLocation(_ sender: Any) {
+        locationManager.requestLocation()
+    }
+}
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let loc = locations.last else { return }
+        currentLocation = loc
+        getWeatherData(latitude: String(currentLocation?.coordinate.latitude ?? 0), longitude: String(currentLocation?.coordinate.longitude ?? 0))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("位置情報取得エラー: \(error)")
     }
 }
 
 extension WeatherViewController {
-    private func getWeatherData(zip: String, country: String) {
+    private func getWeatherData(latitude: String, longitude: String) {
         let API_KEY = "4427c745cb0f68ef58840352e8f43ab2"
-        let urlString = "http://api.openweathermap.org/data/2.5/weather?zip=\(zip),\(country)&units=metric&appid=\(API_KEY)"
+        let urlString = "http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&units=metric&appid=\(API_KEY)"
         guard let encodedUrlString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
         AF.request(encodedUrlString, method: .get, parameters: nil, encoding: JSONEncoding.default).responseData { [weak self] (response) in
             guard let self else { return }
@@ -39,7 +60,7 @@ extension WeatherViewController {
                 guard let taikanTemp = json["main"]["temp"].number else { return }
                 guard let currentHumi = json["main"]["humidity"].number else { return }
                 guard let windVelo = json["wind"]["speed"].number else { return }
-
+                
                 self.max.text = "最高気温: \(Int(truncating: maxTemp).description)℃"
                 self.min.text = "最低気温: \(Int(truncating: minTemp).description)℃"
                 self.taikan.text = "体感温度: \(Int(truncating: taikanTemp).description)℃"
